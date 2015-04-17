58cdb9b73c99fb1ca1052f71262728db833eacbc
Tony DiNitto <think41c@gmail.com>
2015-04-17 16:49:38 -0600

    Added ability to shovel actual data instead of just true/false results

--- strain.rb
+++ strain.rb
@@ -3,10 +3,17 @@ class Array
     
     kept_elements = []
     each do |user_array_element|
-      kept_elements << yield(user_array_element)
+      puts user_array_element
+      if yield(user_array_element) == true 
+        kept_elements << user_array_element
+      else 
+      end
+
     end
-    kept_elements
+    p kept_elements
 
 
   end
 end
+
+[1, 2, 30].keep { |e| e < 10 }
