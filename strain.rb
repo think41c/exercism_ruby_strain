5be3d8ae4eb2c53883ffd38e92611cbd4fed02a1
Tony DiNitto <think41c@gmail.com>
2015-04-17 16:52:57 -0600

    More refactoring with ||= to add clarity.

--- strain.rb
+++ strain.rb
@@ -1,10 +1,11 @@
 class Array
-  def keep
-    
-    kept_elements = []
+
+  def keep 
+    # kept_elements = []
     each do |user_array_element|
       user_array_element
       if yield(user_array_element) == true 
+        kept_elements ||= []
         kept_elements << user_array_element
       end
     end
