d007aaf6694ebbb41ecc0ac718d8cb8f4ab7ea73
Tony DiNitto <think41c@gmail.com>
2015-04-19 23:51:51 -0400

    Refactored discard logic for clarity.

--- strain.rb
+++ strain.rb
@@ -11,10 +11,10 @@ class Array
   end
 
   def discard
+    kept_elements = []
     each do |user_array_element|
       user_array_element
       if yield(user_array_element) == false 
-        kept_elements ||= []
         kept_elements << user_array_element
       end
     end
