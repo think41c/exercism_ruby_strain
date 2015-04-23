02f46489e6d391f542729f4d63d589c45b28e410
Tony DiNitto <think41c@gmail.com>
2015-04-23 19:00:35 -0400

    Pesudo code for major refactor to simplify methods.

--- strain.rb
+++ strain.rb
@@ -1,6 +1,11 @@
+require 'pry'
+
 class Array
 
   def keep
+    # Call a keep/discard method that takes in a /true/false parameter
+    # Then use that param in the "if yield(x) == PARAM" line of the code. 
+    
     kept_elements = []
     each do |user_array_element|
       if yield(user_array_element)
@@ -8,6 +13,7 @@ class Array
       end
     end
     kept_elements
+    binding.pry
   end
 
   def discard
